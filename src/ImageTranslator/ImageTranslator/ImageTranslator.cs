5e0341ed0ecd359ac0dfa49abdcd7ab25876ca42
magnus5552 <isackov.stepa@yandex.ru>
2023-03-25 01:04:44 +0500

    Add ImageTranslator and change gif to jpeg

---
+++ src/ImageTranslator/ImageTranslator/ImageTranslator.cs
@@ -0,0 +1,20 @@
+﻿namespace ImageTranslator;
+
+public class ImageTranslator
+{
+    /// <summary>
+    /// Принимает битовое представление файла jpeg
+    /// </summary>
+    public ImageTranslator(byte[] file)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Записывает биты файла в файл класса на языке jack в директорию destinationPath, название класса - JPEGBytes
+    /// </summary>
+    public void WriteFile(string destinationPath)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/ImageTranslator/ImageTranslator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ImageTranslator;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var path = args[0];
+        var file = File.ReadAllBytes(path);
+        
+        var imageTranslator = new ImageTranslator(file);
+        
+        var destPath = args.Length > 1 ? args[1] : Directory.GetCurrentDirectory();
+        
+        imageTranslator.WriteFile(destPath);
+        
+        // TODO: Далее нужен код, который автоматически запускает JackCompiler и компилирует папку src
+    }
+}
