6c5e461f909c77f535434bdbe18901e956ec34e6
magnus5552 <isackov.stepa@yandex.ru>
2023-04-01 21:05:14 +0500

    Change README.md

--- ImageTranslator.bat
+++ ImageTranslator.bat
@@ -3,13 +3,20 @@
 setlocal
 
 if not "%~1"=="" (
+    if not "%~x1"==".jpeg" (
+        if not "%~x1"==".jpg" (
+            echo Wrong file extension: %~x1
+            echo Only .jpeg or .jpg files supported
+            goto :eof
+        )
+    )
     set "_arg1=%~f1"
 ) else (
-    for %%i in (*.jpeg, *.jpeg) do (
+    for %%i in (*.jpeg, *.jpg) do (
         set _arg1=%%~nxdpi
         goto translate
     )
-    echo No jpeg files in current directory
+    echo No .jpeg or .jpg files in current directory
     goto :eof
 )
 pushd %~dp0
